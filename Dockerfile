f5a4efad59e38f2840f9e3920d1ee006d17d2179
jamoroso <jamoroso@acadian-asset.com>
2023-03-31 10:39:40 -0500

    added seti example with spark

--- Dockerfile
+++ Dockerfile
@@ -1,42 +1,10 @@
-ARG IMAGE=755674844232.dkr.ecr.us-east-1.amazonaws.com/notebook-python/emr-6.10.0:latest
-FROM ${IMAGE}
-
+FROM 755674844232.dkr.ecr.us-east-1.amazonaws.com/spark/emr-6.9.0:latest
 USER root
-
-RUN yum -y update && yum install -y sudo
-
-COPY --chown=hadoop:hadoop . /turboseti
-WORKDIR /turboseti
-
-#RUN cat dependencies.txt | xargs -n 1 yum install -y
-#RUN xargs -n 1 yum install -y < dependencies.txt
-RUN amazon-linux-extras install epel -y
-RUN yum install -y hdf5-devel
-RUN yum install -y python3-pip
-RUN yum install -y yum install python3-devel.x86_64
-RUN yum install -y python3-setuptools
-#RUN yum install -y libhdf5-dev
-RUN yum install -y gcc
-RUN yum install -y gcc-gfortran
-RUN yum install -y wget
-RUN yum install -y curl
+### Add customization commands here ####
 RUN yum install -y git
-
-RUN python3 -m pip install -U pip
-RUN python3 -m pip install git+https://github.com/john-caylent/blimpy
-RUN python3 -m pip install -r requirements.txt
-RUN python3 -m pip install -r requirements_test.txt
-RUN python3 setup.py install
-#RUN cd test && python3 download_test_data.py && cd ..
-# RUN cd test && bash run_tests.sh && cd ..
-
-RUN find test -name "*.h5" -type f -delete
-RUN find test -name "*.log" -type f -delete
-RUN find test -name "*.dat" -type f -delete
-RUN find test -name "*.fil" -type f -delete
-RUN find test -name "*.png" -type f -delete
-RUN find . -path '*/__pycache__*' -delete
+RUN pip3 install requests
+RUN pip3 install git+https://github.com/john-caylent/blimpy
+RUN pip3 install git+https://github.com/john-caylent/turbo_seti
 
 USER hadoop:hadoop
-
-WORKDIR /home/hadoop
+COPY seti_example.py /scripts/seti_example.py
---
+++ seti_example.py
@@ -0,0 +1,86 @@
+import sys
+import os
+import glob
+import time
+import requests
+import boto3
+from blimpy import Waterfall
+from turbo_seti.find_doppler.find_doppler import FindDoppler
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession\
+        .builder\
+        .appName("SETI")\
+        .getOrCreate()
+
+    h5_url = sys.argv[1]
+    s3_bucket = sys.argv[2]
+    s3_prefix = sys.argv[3]
+
+    response = requests.get(h5_url)
+
+    if response.status_code == 200:
+        with open("dataset.h5", 'wb') as f:
+            f.write(response.content)
+        print("File dataset.h5 downloaded successfully!")
+    else:
+        print("Failed to download file")
+        sys.exit(1)
+
+    H5DIR = "./"
+    H5PATH = H5DIR + "dataset.h5"
+
+    # print("\nUsing HDF5 file: {}\nHeader and data shape:".format(H5PATH))
+    # # -- Get a report of header and data shape
+    # wf = Waterfall(H5PATH)
+    # wf.info()
+    # # -- Instantiate FindDoppler.
+    # print("\nInstantiating the FindDoppler object.")
+    # fdop = FindDoppler(datafile=H5PATH, max_drift=4, snr=25,out_dir="./")
+    # # -- Search for hits and report elapsed time.
+    # print("\nBegin doppler search.  Please wait ...")
+    # t1 = time.time()
+    # fdop.search()
+    # elapsed_time = time.time() - t1
+    # print("\nFindDoppler.search() elapsed time = {} seconds".format(elapsed_time))
+
+
+    # glob will create a list of specific files in a directory. In this case, any file ending in .h5.
+    h5list = sorted(glob.glob("./" + '*.h5'))
+
+    # Iterate over the 6 HDF5 files
+    print("tutorial_1: Please wait for the \"End\" message,\n")
+    for file in h5list:
+        # Execute turboSETI in the terminal
+        console = 'turboSETI ' + file + ' -M 4 -s 10 -o ' + "./"
+        os.system(console)
+
+    print("\ntutorial_1: All HDF5 files have been successfully processed.")
+    print("tutorial_1: End.")
+
+    print("tutorial_1: Please wait for the \"End\" message,\n")
+    console = "plotSETI -f 3 -o " + "./" + " " + "./"
+    os.system(console)
+    print("\ntutorial_1: All PNG files have been generated.")
+    print("tutorial_1: End.")
+
+    directory_path = "./"
+    # Create an S3 client
+    s3 = boto3.client('s3')
+    # Set the directory path where the PNG images are located
+
+    # Loop through each file in the directory and upload any PNG images to the S3 bucket
+    for file_name in os.listdir(directory_path):
+        if file_name.endswith('.png'):
+            file_path = os.path.join(directory_path, file_name)
+            with open(file_path, 'rb') as f:
+                s3.upload_fileobj(f, s3_bucket, f'{s3_prefix}/{file_name}')
+                print(f'Successfully uploaded {file_name} to S3 bucket {s3_bucket} in directory {s3_prefix}')
+                url_expiration = 3600 # URL expiration time in seconds (set to 1 hour)
+                url = s3.generate_presigned_url('get_object', Params={'Bucket': s3_bucket, 'Key': f'{s3_prefix}/{file_name}'}, ExpiresIn=url_expiration)
+                print(f'Pre-signed URL for {file_name}: {url}')
+
+    spark.stop()
