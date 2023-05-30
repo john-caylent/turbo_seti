eece1792b258112174b223652a518ed552615970
jamoroso <jamoroso@acadian-asset.com>
2023-05-30 12:31:07 -0500

    modified script

--- spark-python-distributed.py
+++ spark-python-distributed.py
@@ -1,27 +1,43 @@
 from pyspark.sql import SparkSession
-import os
+from pyspark.sql.functions import month, col
+from datetime import datetime
+import sys
 
 if __name__ == "__main__":
 
     spark = SparkSession\
         .builder\
-        .appName("StartupAnalysis")\
+        .appName("StockAnalysis")\
         .getOrCreate()
 
+    dataset_mode = sys.argv[1]
+
+    if dataset_mode == "complete":
+        s3_url = "s3://sandbox-terraform-aws-caylenttwo-emr/dataset/stocks"
+    else:
+        s3_url = "s3://sandbox-terraform-aws-caylenttwo-emr/dataset/stocks-minimal"
+
+
+    # Set a date to use dinamic naming on the output
+    now = datetime.now()
+    datetime_string = now.strftime("%Y%m%d%H%M%S")
+
     # Read a large dataset from a file or any other data source
-    data = spark.read.csv("s3://sandbox-terraform-aws-caylenttwo-emr/dataset/acquisitions.csv", header=True, inferSchema=True)
+    data = spark.read.csv(s3_url, header=True, inferSchema=True)
+
+    # Show schema
+    data.printSchema()
 
     # Perform transformations and actions on the data
-    processed_data = data.filter(data["price_amount"] > 500000000) \
-                        .groupBy("term_code") \
-                        .agg({"price_amount": "avg"}) \
-                        .orderBy("term_code")
+    processed_data = data.filter((col("open") >= 1000) & (col("close") < 1000)) \
+        .groupBy(month(col("date")).alias("month")).count()
 
     # Show the processed data
     processed_data.show()
 
     # Write the processed data to an output file or any other data sink
-    processed_data.write.csv("s3://sandbox-terraform-aws-caylenttwo-emr/dataset/output.csv", header=True)
+    processed_data.write.csv(
+        "s3://sandbox-terraform-aws-caylenttwo-emr/dataset/{}-output.csv".format(datetime_string), header=True)
 
     # Stop the SparkSession
     spark.stop()
